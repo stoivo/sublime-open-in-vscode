a561ef4aeaad1d287f30c60a13f280d3662c94d2
Simon <simon.toivo@telhaug.no>
2020-02-28 18:54:26 +0100

    Improve readability

--- main.py
+++ main.py
@@ -13,19 +13,30 @@ class VscOpenInVisalStudioCodeCommand(WindowCommand):
         global first_time
 
         window = self.window
-        folders = window.folders()
+        folders = window.folders() or []
         settings = sublime.load_settings("VSCode.sublime-settings")
         view = window.active_view()
-        path = view.file_name()
-        sel = view.sel()
-        (row, col) = view.rowcol(sel[0].a)
 
         cmd = [
             settings.get('vscode-path'),
-            '--new-window' if first_time else '',
-            '{}'.format(" ".join('--add ' + "'{}'".format(folder) for folder in folders)) if folders else '',
-            '--reuse-window',
-            '--goto "{}:{}:{}"'.format(path, (row) + 1, col + 1) if path else None
         ]
-        myCmd = " ".join(cmd)
-        os.system(myCmd)
+
+        if first_time:
+            cmd.append('--new-window')
+
+        for folder in folders:
+            cmd.append("--add \"{}\"".format(folder))
+
+        cmd.append('--reuse-window')
+
+        path = view.file_name()
+        sel = view.sel()
+
+        if path:
+            if False and sel:
+                (row, col) = view.rowcol(sel[0].a)
+                cmd.append('--goto "{}":{}:{}'.format(path, (row) + 1, col + 1))
+            else:
+                cmd.append('--goto "{}"'.format(path))
+
+        os.system(" ".join(cmd))
